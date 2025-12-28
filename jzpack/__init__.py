e8ea1cbe93e2e7fa2e257884dac6bb38e3dde41d
Hasan Zaibak <hasan.zaibak@hotmail.com>
2025-12-28 15:43:24 -0300

    fix: package init

--- jzpack/__init__.py
+++ jzpack/__init__.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from .compressor import JZPackCompressor, StreamingCompressor
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 __all__ = [
     "__version__",
     "compress",
